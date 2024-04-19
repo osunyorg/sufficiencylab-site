7f5502c2765bd8027db15bd37bedfd41610fbb65
Marion Jolas <marion.jolas@noesya.coop>
2024-04-19 13:12:21 +0200

    Animation

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -7,41 +7,41 @@ window.typewriter = {
         var i = 0;
         var j = 10;
         var txt = document.querySelector(".lead").textContent;
+        var txts = txt.split('/');
+
+        var k= 0;
         var j = 0;
-        var speed = 75;
-        var txtvide = "";
-        document.querySelector(".lead").innerHTML = txtvide;
-        function typeWriterWrite() {
-              
-            
-            if (i < txt.length) {
-                
+        document.querySelector(".lead").innerHTML = "";
+        var typeWriterWrite = function () {
+            if (i < txts[k].length) {
                 document.querySelector(".lead").innerHTML;
-                document.querySelector(".lead").innerHTML = document.querySelector(".lead").innerHTML + txt.charAt(i);
+                document.querySelector(".lead").innerHTML = document.querySelector(".lead").innerHTML + txts[k].charAt(i);
                 i++;
-                setTimeout(typeWriterWrite, speed);
-            } 
-            else {
-
-                setTimeout(typeWriterWrite, 10000);
-                if (j <= txt.length) {
-                    document.querySelector(".lead").innerHTML = txtvide;
-                    document.querySelector(".lead").innerHTML += txt.slice(0, txt.length-j);
+                setTimeout(typeWriterWrite, 75);
+            }  else {
+                console.log('oui');
+                //setTimeout(typeWriterWrite, 10000);
+                if (j <= txts[k].length) {
+                    console.log(txts[k].length);
+                    document.querySelector(".lead").innerHTML = "";
+                    document.querySelector(".lead").innerHTML += txts[k].slice(0, txts[k].length-j);
+                    document.querySelector(".lead").innerHTML += "_";
                     j++;
-                    setTimeout(typeWriterWrite, 50);
+                    setTimeout(typeWriterWrite, 75);
+                } else {
+                    console.log(k);
+                    k += 1;
+                    i = 0;
+                    j = 0;
+                    if (k > txts.length-1){
+                        k=0;
+                    }
+                    setTimeout(typeWriterWrite, 75);
                 }
-           
             }
-            
-
-        }
-
+        };
         typeWriterWrite();
-        
-        //typeWriterUnWrite();
     },
-
-
 
     invoke: function () {
         "use strict";
